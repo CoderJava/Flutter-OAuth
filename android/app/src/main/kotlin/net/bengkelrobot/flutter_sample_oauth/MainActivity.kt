1f4cfbfc62e9d37e1376b76f2078c466f7f79e8f
Yudi Setiawan <kolonel.yudisetiawan@gmail.com>
2019-12-08 10:59:04 +0700

    Initial commit

---
+++ android/app/src/main/kotlin/net/bengkelrobot/flutter_sample_oauth/MainActivity.kt
@@ -0,0 +1,12 @@
+package net.bengkelrobot.flutter_sample_oauth
+
+import androidx.annotation.NonNull;
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine);
+    }
+}
